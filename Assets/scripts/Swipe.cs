60bc37a2f2b5838d1fac43d73661c751222a8526
CheiJesus09 <xd1destruction@gmail.com>
2026-01-22 20:12:33 -0600

    movimiento de personaje

---
+++ Assets/scripts/Swipe.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Swipe : MonoBehaviour
+{
+    [SerializeField]
+    private bool isActive = true;
+    [SerializeField]
+    private float minSwipeDistance = 50f;
+    [SerializeField]
+    private  UnityEvent onSwipeUp;
+    [SerializeField]
+    private UnityEvent onSwipeDown;
+    [SerializeField]
+    private UnityEvent onSwipeLeft;
+    [SerializeField]
+    private UnityEvent onSwipeRight;
+    private Vector2 startPosition;
+    private void Update()
+    {
+        if (!isActive) return;
+        
+        if (Input.GetMouseButtonUp(0))
+        {
+            startPosition = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            Vector2 endPosition = Input.mousePosition;
+            Vector2 swipeVector = endPosition - startPosition;
+
+            if (swipeVector.magnitude >= minSwipeDistance)
+            {
+                DetecSwipeDorection(swipeVector);
+            }
+        }
+    }
+    private void DetecSwipeDorection(Vector2 swipeVector)
+    {
+        float angle = Vector2.SignedAngle(Vector2.right, swipeVector);
+
+        if (angle >= -45f && angle <= 45f)
+        {
+            onSwipeRight?.Invoke();
+        }
+        else if (angle > 45f && angle < 135f)
+        {
+            onSwipeUp?.Invoke();
+        }
+        else if  (angle >= 135f || angle <= -135f)
+        {
+            onSwipeLeft?.Invoke();
+        }
+        else if (angle < -45f && angle > -135f)
+        {
+            onSwipeDown?.Invoke();
+        }
+
+    }
+}
